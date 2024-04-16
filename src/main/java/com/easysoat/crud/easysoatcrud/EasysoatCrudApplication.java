17340d2035e84684c6defe597255de473491456a
Ramiro Sanchez <sanchezhernandezramiroalberto@gmail.com>
2024-04-16 13:56:21 -0500

    Proyecto de un crud con springboot e hibernate

---
+++ src/main/java/com/easysoat/crud/easysoatcrud/EasysoatCrudApplication.java
@@ -0,0 +1,13 @@
+package com.easysoat.crud.easysoatcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasysoatCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasysoatCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easysoat/crud/easysoatcrud/EasysoatCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easysoat.crud.easysoatcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasysoatCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
